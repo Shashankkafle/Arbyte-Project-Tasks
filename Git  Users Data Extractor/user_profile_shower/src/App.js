aa8031f40fd45f7328aead40dd1613811eab87a4
Shashank <shashank.kaphle@gmail.com>
2022-01-20 16:08:12 +0545

    profile shower initial commit

---
+++ Git  Users Data Extractor/user_profile_shower/src/App.js
@@ -0,0 +1,12 @@
+import { SearchBar } from "./SearchBar";
+import { UserList } from "./UserList";
+function App() {
+  return (
+    <div className="App">
+      <SearchBar></SearchBar>
+      <UserList></UserList>
+    </div>
+  );
+}
+
+export default App;
