35b56004c7de7774d4ece51e20a52fa164f3d1c8
Shashank <shashank.kaphle@gmail.com>
2022-02-03 17:28:10 +0545

    listing uploaded to firebase

--- housemarketplace/src/pages/CreateListing.jsx
+++ housemarketplace/src/pages/CreateListing.jsx
@@ -1,7 +1,12 @@
 import { useState, useEffect, useRef } from 'react'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { db } from '../firebase.config';
+import { v4 as uuidv4 } from 'uuid'
 import { useNavigate } from 'react-router-dom'
 import Spinner from '../components/Spinner'
+import { toast } from 'react-toastify'
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
 
 function CreateListing() {
     const[formData,setFormData] = useState({
@@ -21,9 +26,90 @@ function CreateListing() {
 
     })
     const[loading,setLoading]=useState(false)
-    const onSubmit=(e)=>{
+    const onSubmit= async(e)=>
+    {   
         e.preventDefault()
+        setLoading(true)
+
+        if (discountedPrice >= regularPrice) {
+            setLoading(false)
+            toast.error('Discounted price needs to be less than regular price')
+            return
+          }
+          if (images.length > 6) {
+            setLoading(false)
+            toast.error('Max 6 images')
+            return
+          }
+
+          
+          const storeImage= async(image) =>{
+            return new Promise((resolve,reject)=>{
+                const storage=getStorage()
+                const fileName= `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+
+                const storageRef=ref(storage,'images/'+fileName)
+                
+                const uploadTask = uploadBytesResumable(storageRef,image)
+
+                uploadTask.on(
+                    'state_changed',
+                    (snapshot) => {
+                      const progress =
+                        (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                      console.log('Upload is ' + progress + '% done')
+                      switch (snapshot.state) {
+                        case 'paused':
+                          console.log('Upload is paused')
+                          break
+                        case 'running':
+                          console.log('Upload is running')
+                          break
+                      }
+                    },
+                    (error) => {
+                      reject(error)
+                    },
+                    () => {
+                      // Handle successful uploads on complete
+                      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        resolve(downloadURL)
+                      })
+                    }
+                  )
+                
+
+            })
+            
+            
+                    
+        }
+        const imageUrls = await Promise.all(
+            [...images].map((image) => storeImage(image))
+          ).catch(() => {
+            setLoading(false)
+            toast.error('Images not uploaded')
+            return
+          })
+             
+          const formDataCopy = {
+            ...formData,
+            imageUrls,
+            timestamp: serverTimestamp(),
+          }
+      
+          delete formDataCopy.images
+          console.log(formDataCopy.address)
+          !formDataCopy.offer && delete formDataCopy.discountedPrice
+      
+          const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
+          setLoading(false)
+          toast.success('Listing saved')
+          navigate(`/category/${formDataCopy.type}/${docRef.id}`)
+        
     }
+
     const onMutate=(e)=>{
         let boolean =null
         if(e.target.value==='true'){
@@ -85,6 +171,8 @@ function CreateListing() {
   if(loading){
       return <Spinner/>
   }
+
+ 
   return (
         <div className='profile'>
             <header>
